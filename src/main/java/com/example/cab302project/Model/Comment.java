cc5a20159e87dd6839ea8cf13bf3f93a1e82fe53
Mathew Karunaratne <mathewkarunaratne@gmail.com>
2024-10-09 10:40:12 +1000

    Updated to utilize observer pattern

--- src/main/java/com/example/cab302project/Model/Comment.java
+++ src/main/java/com/example/cab302project/Model/Comment.java
@@ -5,27 +5,46 @@ public class Comment {
     private int postId;
     private String text;
     private String timestamp;
+    private String author;
 
-    public Comment(int commentId, int postId, String text, String timestamp) {
-        this.commentId = commentId;
+    public Comment( int postId, String text, String timestamp, String author) {
         this.postId = postId;
         this.text = text;
         this.timestamp = timestamp;
+        this.author = author;
     }
 
     public int getCommentId() {
         return commentId;
     }
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
 
     public int getPostId() {
         return postId;
+    }
+    public void setPostId(int postId) {
+        this.postId = postId;
     }
 
     public String getText() {
         return text;
     }
+    public void setText(String text) {
+        this.text = text;
+    }
 
     public String getTimestamp() {
         return timestamp;
+    }
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
     }
 }
